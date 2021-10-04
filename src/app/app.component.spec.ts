5bc859054175968fbf38a8b8226b5179716d0469
MarwenShili <eshilimarwen9@gmail.com>
2021-10-04 15:23:09 +0200

    marwen

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MarwenSh'`, () => {
+  it(`should have as title 'PORTf'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MarwenSh');
+    expect(app.title).toEqual('PORTf');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MarwenSh app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('PORTf app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+//import { AngularFontAwesomeModule} from 'angular-font-awesome';
+
+
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import {NgxTypedJsModule} from 'ngx-typed-js';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { MyservicesComponent } from './component/myservices/myservices.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { TeamsComponent } from './component/teams/teams.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ScrollComponent } from './component/scroll/scroll.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    MyservicesComponent,
+    SkillsComponent,
+    TeamsComponent,
+    ContactComponent,
+    FooterComponent,
+    ScrollComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  //  FontAwesomeModule,
+    CarouselModule ,
+    NgxTypedJsModule
+   // AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
